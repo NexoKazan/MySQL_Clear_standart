b504900ade99249d6ace9df9e31038dce353c30d
IAKazantsev <IAKazantsev@DIT-07-230-02>
2018-12-12 16:42:25 +0300

    Первый вариант структуры.

---
+++ MySQL_Clear_standart/SelectStructure.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQL_Clear_standart
+{
+    class SelectStructure
+    {
+        private string _name;
+        private string _output;
+        private string _tableName;
+        private int _id;
+
+
+    }
+}
