b49122a0c452a8bf572c2662b894f291058f8b85
Игорь Казанцев <Rexitt@yandex.ru>
2019-03-16 12:14:01 +0300

    .

---
+++ MySQL_Clear_standart/S_Type.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MySQL_Clear_standart
+{
+    class S_Type
+    {
+        private string _name;
+        private int _size;
+
+    }
+}
